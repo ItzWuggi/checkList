013579327ad9bb7fcee0dbc1a8143ff7506957d7
Antonio Natoli <antonionatoli420@gmail.com>
2024-05-27 11:42:11 +0200

    start checklist

--- resources/views/checkList.blade.php
+++ resources/views/checkList.blade.php
@@ -1,3 +1,17 @@
 <x-layout>
     <x-masthead/>
+    <div class="container1">
+        <div class="toDoApp">
+            <h1 class="text-dark">Lista di cose da fare <img src="/storage/img/icons8-da-fare-50.png" alt=""></h1>
+            <div class="row justify-content-space-between">
+                <input type="text" id="input-box" class="form-control" placeholder="Inserisci il testo">
+                <button class="button">Aggiungi</button>
+            </div>
+            <ul id="list-container">
+                <li>Task 1</li>
+                <li>Task 2</li>
+                <li>Task 3</li>
+            </ul>
+        </div>
+    </div>
 </x-layout>
